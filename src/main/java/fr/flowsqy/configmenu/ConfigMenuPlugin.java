cbf35cb0d59d9696d11aade54d02417ffe5d9c99
Flowsqy <Flo504.Dev@gmail.com>
2021-07-10 19:21:26 +0200

    Init project

---
+++ src/main/java/fr/flowsqy/configmenu/ConfigMenuPlugin.java
@@ -0,0 +1,12 @@
+package fr.flowsqy.configmenu;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ConfigMenuPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
