40918bc43a23e29c539b832ee697e437d9951499
Flowsqy <Flo504.Dev@gmail.com>
2021-07-11 13:51:50 +0200

    Add reload command

---
+++ src/main/java/fr/flowsqy/configmenu/commands/ReloadCommand.java
@@ -0,0 +1,39 @@
+package fr.flowsqy.configmenu.commands;
+
+import fr.flowsqy.configmenu.ConfigMenuPlugin;
+import org.bukkit.command.*;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ReloadCommand implements CommandExecutor {
+
+    public final static TabCompleter EMPTY_TAB_COMPLETER = new EmptyTabCompleter();
+
+    private final String reloadMessage;
+
+    public ReloadCommand(ConfigMenuPlugin plugin, YamlConfiguration messages) {
+        final PluginCommand reloadCommand = plugin.getCommand("configmenureload");
+        assert reloadCommand != null;
+        reloadCommand.setTabCompleter(EMPTY_TAB_COMPLETER);
+        reloadCommand.setExecutor(this);
+        reloadMessage = messages.getString("commands.reload");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        //TODO Link to reload method
+        sender.sendMessage(reloadMessage);
+        return true;
+    }
+
+    // TODO Move it in an appropriate class
+    private final static class EmptyTabCompleter implements TabCompleter {
+        @Override
+        public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+            return Collections.emptyList();
+        }
+    }
+
+}
